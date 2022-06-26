677bc3df47f23edfe70ed82f123843cf9266240a
Erhan <erhantezer68@gmail.com>
2022-06-26 14:37:06 +0300

    updated

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -2,6 +2,8 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.querySelector("#btn"); //? BURASI BUTON
 const color = document.querySelector(".color");   //? BURASI SPAN
 
+
+//? TIKLAMA OLAYLARINDA İSTEDİĞİMİZ OLAYLARIN GERÇEKLEŞME FONKSİYONU 
  btn.addEventListener("click", function () {
     const randomNumber = getRandomNumber();
     document.body.style.backgroundColor= colors[randomNumber];
--- color-flipper/hex.js
+++ color-flipper/hex.js
@@ -1,9 +1,14 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+//? kullanacağımız yerlerin seçilip değişkene atanması
 const btn =document.getElementById("btn");
 const color = document.querySelector(".color");
 
+//? tıklama esnasındaki eklemeleri dinleme yapmak ve ekleme işi
 btn.onclick = function () {
   let hexColor = "#"  ;
+
+  //?  HEX KODLARI 6 TANE OLDUĞU İÇİN FOR DÖNGÜSÜ İLE HEXCOLOR A 6 SAYI GÖNDERMEK İÇİN FOR DÖNGÜSÜ KULLANDIM
   for( let i =0; i<6;i++){
   hexColor += hex[getRandomNumber()]
 }
@@ -11,6 +16,8 @@ color.textContent=hexColor;
 document.body.style.backgroundColor =hexColor;
 }
 
+//? fonksiyon onclick fonksiyonu içinede yazabilirdik best practice si bu şekilde dir 
+//? KULLANMA AMACIMIZ HEX SAYISI KADAR RASTGELE SAYI ÜRETMEK
 function getRandomNumber() {
     return Math.floor(Math.random() * hex.length)
 }
