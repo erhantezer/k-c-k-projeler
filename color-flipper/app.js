77fb047dd94b2dd1b55f521315af392832a4ed67
Erhan <erhantezer68@gmail.com>
2022-06-26 14:28:29 +0300

    new small project was added

---
+++ color-flipper/app.js
@@ -0,0 +1,15 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.querySelector("#btn"); //? BURASI BUTON
+const color = document.querySelector(".color");   //? BURASI SPAN
+
+ btn.addEventListener("click", function () {
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor= colors[randomNumber];
+    color.innerText = colors[randomNumber]
+ })
+
+ function getRandomNumber () {
+    return Math.floor(Math.random()*colors.length)
+ }
+
+
