ac1481d4467a352db63b50ad4905a6caba5b5f6e
Erhan <erhantezer68@gmail.com>
2022-06-27 19:25:56 +0300

    updated

--- counter/app.js
+++ counter/app.js
@@ -2,15 +2,21 @@ let count = 0;
 
 const value =document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
-// console.log(btns);
+console.log(btns);
 
+
+//? forEach ile btns array [button.btn.decrease, button.btn.reset, button.btn.increase] içine girilecek çünkü
+//? q.selectorAll ile seçildiği için nodeList döndürdü
 btns.forEach(function (btn) {
 
+//? herbir öge tıklandığında oluşacak olay için fonksiyon tanımlanmıştır
     btn.addEventListener("click", function (event){
     // console.log(e);
+//? tıklanılan ögede bir olay oluşacak bu olay hangisine tıklandıysa onu ortaya çıkarma buradaki olay  class larını bulma event i
     const styles = event.target.classList;
-    console.log(styles);
+    // console.log(styles);
 
+//? burada hangi ögeye tıklandıysa hangi kelimeyi bulursa o eşleşecek ve count olayı oluşacak
     if (styles.contains("decrease")){
         count--;
     } else if (styles.contains("increase")){
@@ -18,6 +24,7 @@ btns.forEach(function (btn) {
     } else if (styles.contains("reset")) {
             count = 0;
     }
+
 
     if (count > 0) {
         value.style.color = "green";
